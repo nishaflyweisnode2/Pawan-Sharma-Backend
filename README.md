258670e0ae72cadecad246cdf65f39593d91278c
princekumar2022 <pk279183@gmail.com>
2023-09-29 19:27:25 +0530

    29 th sept

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# Pawan-Sharma-Backend" 
+
---
+++ models/userModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+    },
+    mobileNumber: {
+        type: String,
+    },
+    image: {
+        type: String,
+    },
+    password: {
+        type: String,
+    },
+    otp: {
+        type: String,
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    userType: {
+        type: String,
+        enum: ["Admin", "User"], default: "User"
+    },
+
+}, { timestamps: true });
+
+
+const User = mongoose.model('User', userSchema);
+
+
+
+module.exports = User;
